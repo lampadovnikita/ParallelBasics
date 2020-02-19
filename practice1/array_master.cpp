6fbf2d01fd53acf7a24ad17fbc341bb5251556b1
Nikita Lampadov <lampadovnikita@gmail.com>
2020-02-19 11:21:53 +0700

    + выбор размера массива + мелкие поправки

--- practice1/array_master.cpp
+++ practice1/array_master.cpp
@@ -7,8 +7,6 @@
 
 using namespace std;
 
-const unsigned int ARRAY_LENGTH = 10;
-
 // Структура, передаваемая в качестве аргумента потока
 struct thread_args {
 	float *number_ptr;
@@ -28,7 +26,6 @@ void* thread_job(void* arg)
 		cout << "No args to work" << endl;
 		return NULL;
 	} else {
-        //cout << "Input number: " <<*(param->number_ptr) << endl;
         *param->number_ptr = param->func_ptr(param->number_ptr);
     }
 	
@@ -47,20 +44,23 @@ void initialize_array(unsigned int length, float *arr) {
 
 int main()
 {
-	int err;
+    unsigned int array_length;
+    cout << "Введите число потоков:";
+    cin >> array_length;
 
-    pthread_t* thread_arr = new pthread_t[ARRAY_LENGTH];
+
+    int err;
+
+    pthread_t* thread_arr = new pthread_t[array_length];
     
-    float* arr = new float[ARRAY_LENGTH];
-    initialize_array(ARRAY_LENGTH, arr);
+    float* number_arr = new float[array_length];
+    initialize_array(array_length, number_arr);
     
-    thread_args* targs_arr = new thread_args[ARRAY_LENGTH];
+    thread_args* targs_arr = new thread_args[array_length];
     
-    for (unsigned int i = 0; i < ARRAY_LENGTH; i++) {
-        
-        //thread_args targs;
+    for (unsigned int i = 0; i < array_length; i++) {
         targs_arr[i].func_ptr = &executable_function;
-        targs_arr[i].number_ptr = arr + i;
+        targs_arr[i].number_ptr = number_arr + i;
 
         err = pthread_create(thread_arr + i, NULL, thread_job, (void*) (targs_arr + i));
         if (err != 0) {
@@ -69,15 +69,16 @@ int main()
 		}
     }
 
-    for (unsigned int i = 0; i < ARRAY_LENGTH; i++) {
+    for (unsigned int i = 0; i < array_length; i++) {
         pthread_join(thread_arr[i], NULL);
 
-        cout << "arr[" << i << "] = " << arr[i] << endl;
+        cout << "arr[" << i << "] = " << number_arr[i] << endl;
     }
     
-    delete[] arr;
+    delete[] number_arr;
     delete[] targs_arr;
-
+    delete[] thread_arr;
+    
 	pthread_exit(NULL);
     
     
