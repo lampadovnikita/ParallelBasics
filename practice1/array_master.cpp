043bf74ce024bfa0e591d5671c5b20c38aa5b9fc
Nikita Lampadov <lampadovnikita@gmail.com>
2020-02-24 02:17:16 +0700

    + задание размера массива через аргументы программы; + задание кол-ва потоков через аргументы программы;

--- practice1/array_master.cpp
+++ practice1/array_master.cpp
@@ -9,8 +9,9 @@ using namespace std;
 
 // Структура, передаваемая в качестве аргумента потока
 struct thread_args {
-	float *number_ptr;
-    float (*func_ptr)(float*);
+	float* first_number_ptr;
+    unsigned int batch_size;
+    float (*func_ptr)(float);
 };
 
 // Функция, которую будет исполнять созданный поток
@@ -19,21 +20,23 @@ void* thread_job(void* arg)
     int err;
 
 	// Преобразуем аргументы к нужному типу
-	thread_args *param = (thread_args *) arg;
+	thread_args* param = (thread_args*) arg;
 	
     // Если не было передано аргументов, то завершаем работу потока
 	if (param == NULL) {
 		cout << "No args to work" << endl;
 		return NULL;
 	} else {
-        *param->number_ptr = param->func_ptr(param->number_ptr);
+        for (unsigned int i = 0; i < param->batch_size; i++) {
+            *(param->first_number_ptr + i) = param->func_ptr(*(param->first_number_ptr + i));
+        }
     }
 	
 	return NULL;
 }
 
-float executable_function(float* input_number) {
-    return sqrt(*input_number);
+float executable_function(float input_number) {
+    return sqrt(input_number);
 }
 
 void initialize_array(unsigned int length, float *arr) {
@@ -42,25 +45,38 @@ void initialize_array(unsigned int length, float *arr) {
     }
 }
 
-int main()
-{
-    unsigned int array_length;
-    cout << "Введите число потоков:";
-    cin >> array_length;
+int main(int argc, char* argv[]) {
+    
+    if (argc < 3) {
+		cout << "Not enough arguments" << endl;
+		return 0;
+	}
 
+    unsigned int array_length = atoi(argv[1]);
+    unsigned int threads_count = atoi(argv[2]);
 
-    int err;
+    if (threads_count > array_length) {
+        threads_count = array_length;
+    }
 
-    pthread_t* thread_arr = new pthread_t[array_length];
+    unsigned int batch_size = array_length / threads_count;
+    unsigned int rest = array_length % threads_count;
+
+    pthread_t* thread_arr = new pthread_t[threads_count];
     
     float* number_arr = new float[array_length];
     initialize_array(array_length, number_arr);
     
-    thread_args* targs_arr = new thread_args[array_length];
+    thread_args* targs_arr = new thread_args[threads_count];
     
-    for (unsigned int i = 0; i < array_length; i++) {
+    int err;
+    for (unsigned int i = 0; i < threads_count; i++) {
         targs_arr[i].func_ptr = &executable_function;
-        targs_arr[i].number_ptr = number_arr + i;
+        targs_arr[i].first_number_ptr = number_arr + i * batch_size;
+        targs_arr[i].batch_size = batch_size;
+        if (i == threads_count - 1) {
+            targs_arr[i].batch_size += rest;
+        }
 
         err = pthread_create(thread_arr + i, NULL, thread_job, (void*) (targs_arr + i));
         if (err != 0) {
@@ -69,9 +85,11 @@ int main()
 		}
     }
 
-    for (unsigned int i = 0; i < array_length; i++) {
+    for (unsigned int i = 0; i < threads_count; i++) {
         pthread_join(thread_arr[i], NULL);
-
+    }
+    
+    for (unsigned int i = 0; i < array_length; i++) {
         cout << "arr[" << i << "] = " << number_arr[i] << endl;
     }
     
