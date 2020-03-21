28b9343a2ebc967c180f9e22dcac6ab3a1c89a57
Nikita Lampadov <lampadovnikita@gmail.com>
2020-03-21 16:53:45 +0700

    + форматирование

--- practice3/mandelbrot.cpp
+++ practice3/mandelbrot.cpp
@@ -17,34 +17,41 @@
  int main()
  {
           /* screen ( integer) coordinate */
-        int iX,iY;
+        int iX;
+        int iY;
+
         const int iXmax = 800; 
         const int iYmax = 800;
        
         /* world ( double) coordinate = parameter plane*/
-        double Cx,Cy;
-        const double CxMin=-2.5;
-        const double CxMax=1.5;
-        const double CyMin=-2.0;
-        const double CyMax=2.0;
+        double Cx;
+        double Cy;
         
-        double PixelWidth=(CxMax-CxMin)/iXmax;
-        double PixelHeight=(CyMax-CyMin)/iYmax;
+        const double CxMin = -2.5;
+        const double CxMax =  1.5;
+        const double CyMin = -2.0;
+        const double CyMax =  2.0;
+        
+        double PixelWidth  = (CxMax - CxMin) / iXmax;
+        double PixelHeight = (CyMax - CyMin) / iYmax;
         
         /* color component ( R or G or B) is coded from 0 to 255 */
         /* it is 24 bit color RGB file */
-        const int MaxColorComponentValue=255; 
+        const int MaxColorComponentValue = 255; 
         
         FILE * fp;
-        char *filename="new1.ppm";
-        char *comment="# ";/* comment should start with # */
+        char *filename = "new1.ppm";
+        char *comment = "# ";/* comment should start with # */
         
         static unsigned char color[3];
         
         /* Z=Zx+Zy*i  ;   Z0 = 0 */
-        double Zx, Zy;
-        double Zx2, Zy2; /* Zx2=Zx*Zx;  Zy2=Zy*Zy  */
+        double Zx;
+        double Zy;
         
+        double Zx2; /* Zx2=Zx*Zx;  Zy2=Zy*Zy  */
+        double Zy2;
+
         int Iteration;
         const int IterationMax = 200;
         
@@ -53,7 +60,7 @@
         double ER2 = EscapeRadius * EscapeRadius;
         
         /*create new file,give it a name and open it in binary mode  */
-        fp= fopen(filename,"wb"); /* b -  binary mode */
+        fp = fopen(filename, "wb"); /* b -  binary mode */
         
         /*write ASCII header to the file*/
         fprintf(fp, "P6\n %s\n %d\n %d\n %d\n", comment, iXmax, iYmax, MaxColorComponentValue);
@@ -61,23 +68,26 @@
         /* compute and write image data bytes to the file*/
         for (iY = 0; iY < iYmax; iY++)
         {
-            Cy = CyMin + iY*PixelHeight;
+            Cy = CyMin + iY * PixelHeight;
             if (fabs(Cy) < PixelHeight / 2)
                 Cy = 0.0; /* Main antenna */
             
             for (iX = 0; iX < iXmax; iX++)
             {         
                 Cx = CxMin + iX * PixelWidth;
+                
                 /* initial value of orbit = critical point Z= 0 */
                 Zx = 0.0;
                 Zy = 0.0;
+
                 Zx2 = Zx * Zx;
                 Zy2 = Zy * Zy;
                 
-                for (Iteration = 0; Iteration < IterationMax && ((Zx2+Zy2) < ER2); Iteration++)
+                for (Iteration = 0; Iteration < IterationMax && ((Zx2 + Zy2) < ER2); Iteration++)
                 {
                     Zy = 2 * Zx * Zy + Cy;
                     Zx = Zx2 - Zy2 + Cx;
+                    
                     Zx2 = Zx * Zx;
                     Zy2 = Zy * Zy;
                 }
@@ -94,8 +104,8 @@
                 { 
                     /* exterior of Mandelbrot set = white */
                     color[0] = 255; /* Red*/
-                    color[1] = 255;  /* Green */ 
-                    color[2] = 255;/* Blue */
+                    color[1] = 255; /* Green */ 
+                    color[2] = 255; /* Blue */
                 }
 
                 /*write color to the file*/
